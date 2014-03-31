679a3162accb2381ad7fc94263726dfcfb0ea659
Quietess <rofleclipse@gmail.com>
2014-03-31 05:17:31 -0400

    oops didnt look through speaktoscrolls
    okay hopefully last clue scroll commit for a while

--- vscape Server/src/com/rs2/model/content/treasuretrails/SpeakToScrolls.java
+++ vscape Server/src/com/rs2/model/content/treasuretrails/SpeakToScrolls.java
@@ -43,7 +43,7 @@ public class SpeakToScrolls {
 		NPC_26(new String[]{"Identify the back of this ", "over-acting brother. ", "(He's a long way from ", "home.)"}, 3574, 1008, "Puzzle", 3),
 		NPC_27(new String[]{"If a man carried my ", "burden, he would break ", "his back. I am not rich, ", "but leave silver in my ", "track. Speak to the ", "keeper of my trail."}, 3575, 558, "", 3),
 		NPC_28(new String[]{"'Small Shoe.' Often found with ", "rod on mushroom."}, 3577, 162, "Puzzle", 3),
-		NPC_29(new String[]{"Snah? I feel all confused, like ", "one of those cakes."}, 3579, 0, "", 3),
+		//NPC_29(new String[]{"Snah? I feel all confused, like ", "one of those cakes."}, 3579, 0, "", 3),
 		NPC_30(new String[]{"This aviator is at the ", "peak of his profession"}, 3580, 170, "Puzzle", 3),
 		NPC_31(new String[]{"Citric Cellar", ""}, 2792, 603, "Puzzle", 3),
 		NPC_32(new String[]{"Generally ", "speaking, his ", "nose was very ", "bent"}, 2783, 296, "Puzzle", 3),
