d5b8efb9b3638b15b72e08c1ae202cab734bf5aa
Bobster3d <Bobster3d@users.noreply.github.com>
2014-04-02 13:17:38 +0100

    Revert "run / compiler bat changes"
    This reverts commit c2646f4f209efdd3a283efc8b33b17e50bb0c83f.

--- vscape Server/Compile.bat
+++ vscape Server/Compile.bat
@@ -1,16 +1,2 @@
 @echo off
-set lib=%lib%bin;
-set lib=%lib%libs/gson-2.2.4.jar;
-set lib=%lib%libs/commons-compress-1.0.jar;
-set lib=%lib%libs/javac++.jar;
-set lib=%lib%libs/jdom-2.0.2.jar;
-set lib=%lib%libs/jruby.jar;
-set lib=%lib%libs/mysql-connector-java-5.1.20-bin.jar;
-set lib=%lib%libs/xpp3-1.1.4c.jar;
-set lib=%lib%libs/xstream-1.4.1.jar;
-set lib=%lib%build/classes/model/content/skills/magic/;
-
-echo Compiling Server...
-
-javac -cp %lib% -d ./bin/ -sourcepath src ./src/com/rs2/model/content/combat/*.java
-pause
+"C:\Program Files\Java\jdk1.8.0\bin\javac.exe"
