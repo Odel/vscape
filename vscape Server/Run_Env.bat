c2646f4f209efdd3a283efc8b33b17e50bb0c83f
Bobster3d <Bobster3d@users.noreply.github.com>
2014-04-02 13:03:32 +0100

    run / compiler bat changes
    the bat compiler should work compiler everything SHOULD

--- vscape Server/Run_Env.bat
+++ vscape Server/Run_Env.bat
@@ -1,4 +1,16 @@
 @echo off
 echo /v/scape
-java -cp bin;libs/gson-2.2.4.jar;libs/commons-compress-1.0.jar;libs/javac++.jar;libs/jdom-2.0.2.jar;libs/jruby.jar;libs/mysql-connector-java-5.1.20-bin.jar;libs/xpp3-1.1.4c.jar;libs/xstream-1.4.1.jar;build/classes/model/content/skills/magic/; com.rs2.Server
+set lib=%lib%bin;
+set lib=%lib%libs/gson-2.2.4.jar;
+set lib=%lib%libs/commons-compress-1.0.jar;
+set lib=%lib%libs/javac++.jar;
+set lib=%lib%libs/jdom-2.0.2.jar;
+set lib=%lib%libs/jruby.jar;
+set lib=%lib%libs/mysql-connector-java-5.1.20-bin.jar;
+set lib=%lib%libs/xpp3-1.1.4c.jar;
+set lib=%lib%libs/xstream-1.4.1.jar;
+set lib=%lib%build/classes/model/content/skills/magic/;
+
+
+java -cp %lib% com.rs2.Server
 pause
