727dcbffce4a52691918c7fbb9e7dac43715e823
Bobster3d <Bobster3d@users.noreply.github.com>
2014-04-25 20:49:57 +0100

    churn fix

--- vscape Server/src/com/rs2/model/content/skills/cooking/DairyChurn.java
+++ vscape Server/src/com/rs2/model/content/skills/cooking/DairyChurn.java
@@ -18,29 +18,27 @@ public class DairyChurn {
 	private static final int CHURN_ANIMATION = 894;
 
 	public static enum ChurnData {
-		CREAM(59238, new int[] { 1927 }, 2130, 21, 18), BUTTER(59239, new int[] { 1927, 2130 }, 6697, 38, 40), CHEESE(59240, new int[] { 1927, 2130, 6697 }, 1985, 48, 64);
+		CREAM(59238, 1927, 2130, 21, 18), 
+		BUTTER(59239, 1927, 6697, 38, 40),
+		CHEESE(59240, 6697, 1985, 48, 64);
 
 		private int buttonId;
-		private int[] used;
+		private int item;
 		private int result;
 		private int level;
 		private double experience;
-
-		public static HashMap<Integer, ChurnData> churnItems = new HashMap<Integer, ChurnData>();
-
-		public static ChurnData forId(int id) {
-			return churnItems.get(id);
-		}
-
-		static {
-			for (ChurnData data : ChurnData.values()) {
-				churnItems.put(data.buttonId, data);
+		
+		public static ChurnData forId(int buttonId) {
+			for (ChurnData churnData : ChurnData.values()) {
+				if (buttonId == churnData.buttonId)
+					return churnData;
 			}
+			return null;
 		}
 
-		private ChurnData(int buttonId, int[] used, int result, int level, double experience) {
+		private ChurnData(int buttonId, int item, int result, int level, double experience) {
 			this.buttonId = buttonId;
-			this.used = used;
+			this.item = item;
 			this.result = result;
 			this.level = level;
 			this.experience = experience;
@@ -50,8 +48,8 @@ public class DairyChurn {
 			return buttonId;
 		}
 
-		public int[] getUsed() {
-			return used;
+		public int getItem() {
+			return item;
 		}
 
 		public int getResult() {
@@ -70,58 +68,44 @@ public class DairyChurn {
 
 	public static boolean churnItem(final Player player, int buttonId) {
 		final ChurnData churnData = ChurnData.forId(buttonId);
-
 		if (churnData == null)
 			return false;
 		if (player.getStatedInterface() == "dairyChurn") {
+			player.getActionSender().removeInterfaces();
 			if (player.getSkill().getLevel()[Skill.COOKING] < churnData.getLevel()) {
 				player.getDialogue().sendStatement("You need a cooking level of " + churnData.getLevel() + " to make this.");
 				return true;
 			}
-
-			player.getActionSender().removeInterfaces();
-
-			final int task = player.getTask();
-			player.setSkilling(new CycleEvent() {
-				boolean hasItems = true;
-
-				@Override
-				public void execute(CycleEventContainer container) {
-					if (!player.checkTask(task)) {
-						player.setTempInteger(0);
-						container.stop();
-						return;
-					}
-					hasItems = true;
-					for (int i = 0; i <= churnData.getUsed().length - 1; i++) {
-						if (!player.getInventory().getItemContainer().contains(churnData.getUsed()[i]))
-							hasItems = false;
-					}
-					container.setTick(5);
-
-					if (!hasItems) {
-						player.getDialogue().sendStatement("You don't have the required items to use the churn.");
-						container.stop();
-						return;
-					}
-					player.getUpdateFlags().sendAnimation(CHURN_ANIMATION);
-					player.getActionSender().sendMessage("You make a " + ItemDefinition.forId(churnData.getResult()).getName().toLowerCase() + ".");
-					for (int i = 0; i < churnData.getUsed().length; i++)
-						player.getInventory().removeItem(new Item(churnData.getUsed()[i]));
-					player.getInventory().addItem(new Item(churnData.getResult()));
-					player.getInventory().addItem(new Item(FlourRelated.BUCKET));
-					player.getSkill().addExp(Skill.COOKING, churnData.getExperience());
-
-				}
-
-				@Override
-				public void stop() {
-					player.resetAnimation();
-				}
-			});
-			CycleEventHandler.getInstance().addEvent(player, player.getSkilling(), 1);
-			return true;
+			if (!player.getInventory().getItemContainer().contains(churnData.getItem())) {
+				player.getDialogue().sendStatement("You need a " + new Item(churnData.getItem()).getDefinition().getName().toLowerCase() + " to make this");
+				return true;
+			}
 		}
-		return false;
+		
+		final int task = player.getTask();
+		player.setSkilling(new CycleEvent() {
+
+			@Override
+			public void execute(CycleEventContainer container) {
+
+				if (!player.checkTask(task) || !player.getInventory().getItemContainer().contains(churnData.getItem())) {
+					container.stop();
+					return;
+				}
+				player.getUpdateFlags().sendAnimation(CHURN_ANIMATION);
+				player.getInventory().removeItem(new Item(churnData.getItem()));
+				player.getInventory().addItem(new Item(churnData.getResult()));
+				player.getSkill().addExp(Skill.COOKING, churnData.getExperience());
+				player.getActionSender().sendMessage("You make a " + ItemDefinition.forId(churnData.getResult()).getName().toLowerCase() + ".");
+				container.stop();
+			}
+
+			@Override
+			public void stop() {
+				player.resetAnimation();
+			}
+		});
+		CycleEventHandler.getInstance().addEvent(player, player.getSkilling(), 1);
+		return true;
 	}
 }
