43e49764af6817e2bf8c8f3f446ef614b00e37fe
vscapeGodDammit <irkthemaw@hotmail.com>
2014-08-22 20:03:50 -0500

    Uncommented HP/Prayer displays
    Requires a new client download for the new cache in order to use ::showhp

--- vscape Server/src/com/rs2/model/players/Player.java
+++ vscape Server/src/com/rs2/model/players/Player.java
@@ -384,7 +384,7 @@ public class Player extends Entity {
     private int genieSelect;
     private boolean hideYell = false;
     private boolean hideColors = false;
-    //public boolean showHp = false;
+    public boolean showHp = false;
 	public Object[][] questData = {
 	// questName, currentStage, finishedStage, questPoints
 	{"Getting Started", 0, 2, 1}};
@@ -1840,9 +1840,9 @@ public class Player extends Entity {
 		    getActionSender().sendMessage("Your hp is at " + (getSkill().getLevel()[Skill.HITPOINTS] -  Integer.parseInt(args[0]))+ ".");
 		    skill.refresh();
 		}
-		/*else if (keyword.equals("showhp")) {
+		else if (keyword.equals("showhp")) {
 			setShowHp(!showHp,true);
-                }*/
+                }
 		else if (keyword.equals("mypos")) {
 			getActionSender().sendMessage("You are at: " + getPosition());
 		}
@@ -5931,7 +5931,7 @@ public class Player extends Entity {
 			}
 		}
 	}
-    /*public boolean getShowHp(){
+    public boolean getShowHp(){
         return showHp;
     }
         public void setShowHp(boolean show, boolean msg) {
@@ -5944,7 +5944,7 @@ public class Player extends Entity {
 						"You have turned off your HP and Prayer display.");
 			}
 		
-	}*/
+	}
         public void setClanChannel(String channel) {
 		String inChannel = channel;
                 
