7d4e71e893a13792ccce7a53115c904defaf4881
Odel <travis.cthall@gmail.com>
2015-01-01 00:55:00 -0800

    update script pt 1

--- tools/update.py
+++ tools/update.py
@@ -4,7 +4,9 @@ print "-=vidyascape updater=-"
 print ""
 
 print "-pulling code (this takes a while)"
- 
+
+os.chdir('/tmp')
+
 DIR_NAME = "vidyascape"
 REMOTE_URL = "https://github.com/Odel/vscape.git"
  
@@ -21,49 +23,49 @@ origin.pull(origin.refs[0].remote_head)
 print "-latest code pulled from GitHub"
 
 print "-compiling code"
-os.mkdir("/home/travis/vidyascape/vscape Server/bin")
-os.chdir('/home/travis/vidyascape/vscape Server')
-os.system('./build.sh')
+#os.mkdir("/tmp/vidyascape/vscape Server/bin")
+#os.chdir('/tmp/vidyascape/vscape Server')
+#os.system('./build.sh')
 
-buildconfirm = raw_input('Did everything build successfully? y/n:')
+#buildconfirm = raw_input('Did everything build successfully? y/n:')
 
-if buildconfirm == "y":
-	print "Shut down the server at this time with ::update 300"
+#if buildconfirm == "y":
+	#print "Shut down the server at this time with ::update 300"
 
-	shutdownconfirm = raw_input('Is the server down now? y/n:')
+	#shutdownconfirm = raw_input('Is the server down now? y/n:')
 
-	if shutdownconfirm == "y":
-		print "-copying data files to server dir"
-		os.chdir('/home/travis/vidyascape/vscape Server/data')
+	#if shutdownconfirm == "y":
+		#print "-copying data files to server dir"
+		#os.chdir('/home/travis/vidyascape/vscape Server/data')
 
-		shutil.rmtree("/var/vidyascape/data/content")
-		shutil.copytree("content","/var/vidyascape/data")
+		#shutil.rmtree("/var/vidyascape/data/content")
+		#shutil.copytree("content","/var/vidyascape/data")
 
-		shutil.rmtree("/var/vidyascape/data/npcs")
-		shutil.copytree("npcs","/var/vidyascape/npcs")
+		#shutil.rmtree("/var/vidyascape/data/npcs")
+		#shutil.copytree("npcs","/var/vidyascape/npcs")
 
-		shutil.rmtree("/var/vidyascape/data/world")
-		shutil.copytree("world","/var/vidyascape/world")
+		#shutil.rmtree("/var/vidyascape/data/world")
+		#shutil.copytree("world","/var/vidyascape/world")
 
-		shutil.copyfile("info.txt","/var/vidyascape/data/info.txt")
-		shutil.copyfile("/home/travis/vidyascape/vscape Server/data/ruby/npc-combat.rb","/var/vidyascape/data/ruby/npc-combat.rb");
-		shutil.copyfile("/home/travis/vidyascape/vscape Server/data/patchnotes.txt","/var/vidyascape/data/patchnotes.txt");
+		#shutil.copyfile("info.txt","/var/vidyascape/data/info.txt")
+		#shutil.copyfile("/home/travis/vidyascape/vscape Server/data/ruby/npc-combat.rb","/var/vidyascape/data/ruby/npc-combat.rb");
+		#shutil.copyfile("/home/travis/vidyascape/vscape Server/data/patchnotes.txt","/var/vidyascape/data/patchnotes.txt");
 
-		os.chdir('/home/travis/vidyascape/vscape Server')
+		#os.chdir('/home/travis/vidyascape/vscape Server')
 
-		shutil.rmtree("/var/vidyascape/datajson")
-		shutil.copytree("datajson","/var/vidyascape/datajson")
+		#shutil.rmtree("/var/vidyascape/datajson")
+		#shutil.copytree("datajson","/var/vidyascape/datajson")
 
-		print "-copying bin to server dir"
+		#print "-copying jar to server dir"
 
-		shutil.rmtree("/var/vidyascape/bin")
-		shutil.copytree("/home/travis/vidyascape/vscape Server/bin","/var/vidyascape/bin")
-		print "-starting server"
-		os.chdir('/var/vidyascape')
-		subprocess.call(["screen", "-d","-m","./run.sh"])
+		#shutil.rmtree("/var/vidyascape/bin")
+		#shutil.copyfile("/home/travis/vidyascape/vscape Server/bin","/var/vidyascape/bin")
+		#print "-starting server"
+		#os.chdir('/var/vidyascape')
+		#subprocess.call(["screen", "-d","-m","./run.sh"])
 		
-		os.chdir('/home/travis')
-		shutil.rmtree(DIR_NAME)
-		print "-done"
+		#os.chdir('/home/travis')
+		#shutil.rmtree(DIR_NAME)
+		#print "-done"
 
 
