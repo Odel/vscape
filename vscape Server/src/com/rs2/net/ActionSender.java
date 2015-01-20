05a25137d69c554d091a507f9b390ebc6a2efe4d
Odel <travis.cthall@gmail.com>
2015-01-20 15:28:07 -0800

    Shameless donation link
    God have mercy on my soul.

--- vscape Server/src/com/rs2/net/ActionSender.java
+++ vscape Server/src/com/rs2/net/ActionSender.java
@@ -109,6 +109,7 @@ public class ActionSender {
 		sendIgnoreList(player.getIgnores());
 		sendMessage("Welcome to /v/scape. There are currently " + World.playerAmount() + " players online.", true);
 		sendMessage("Before you ask a question, check ::info and/or ::patchnotes.", true);
+		sendMessage("Feel like reimbursing Odel for server costs?  Go to odelvidyascape.blogspot.com", true);
 		//QPEdit(player.getQuestPoints());
 		return this;
 	}
