db84cedd9dcb5d54893de45d6aa514a169a93891
Pickles- <dill.pickle2@yahoo.com>
2015-01-30 17:33:40 -0500

    Wilderness agility course gates fix?

--- vscape Server/src/com/rs2/model/objects/functions/Doors.java
+++ vscape Server/src/com/rs2/model/objects/functions/Doors.java
@@ -67,6 +67,10 @@ public class Doors {
 		{
 			return false;
 		}
+		if((id == 2307 || id == 2308) && x >= 2997 && x <= 2998 && y == 3931)
+		{
+			return false;
+		}
 		if((id == 52 || id == 53) && x >= 2649 && x <= 2650 && y == 3470)
 		{
 			return false;
