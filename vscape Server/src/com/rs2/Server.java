185d94a15224b7772e9fbae902b81b16c2972f22
Pickles- <dill.pickle2@yahoo.com>
2015-03-11 23:41:41 -0400

    scripted edgelord fags in burthorpe

--- vscape Server/src/com/rs2/Server.java
+++ vscape Server/src/com/rs2/Server.java
@@ -10,6 +10,7 @@ import com.rs2.model.content.consumables.Food;
 import com.rs2.model.content.minigames.magetrainingarena.*;
 import com.rs2.model.content.quests.impl.MonkeyMadness.ApeAtollNpcs;
 import com.rs2.model.content.quests.QuestHandler;
+import com.rs2.model.content.quests.impl.DeathPlateau.BurthorpeCampHandler;
 import com.rs2.model.content.skills.fishing.FishingSpots;
 import com.rs2.model.npcs.Npc;
 import com.rs2.model.npcs.NpcDefinition;
@@ -232,6 +233,7 @@ public class Server implements Runnable {
 
 			NpcLoader.loadAutoSpawn("./data/npcs/spawn-config.cfg");
 			ApeAtollNpcs.init();
+			BurthorpeCampHandler.init();
 
             HighscoresManager.load();
             
