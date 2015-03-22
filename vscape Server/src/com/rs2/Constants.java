95389055e98c18fea3a01790169582412e2b65ea
Bobster3d <Bobster3d@users.noreply.github.com>
2015-03-22 09:32:23 +0000

    DM
    why do they wear they mask

--- vscape Server/src/com/rs2/Constants.java
+++ vscape Server/src/com/rs2/Constants.java
@@ -68,7 +68,7 @@ public class Constants {
 	public static final int MAX_NPC_ID = 6391;
 	public static final int NPC_WALK_DISTANCE = 2;
 	public static final int NPC_FOLLOW_DISTANCE = 10;
-	public static final int MAX_ITEMS = 11790;
+	public static final int MAX_ITEMS = 11792;
 	public static final int MAX_ITEM_COUNT = Integer.MAX_VALUE;
 	public static final int MAX_PLAYERS_AMOUNT = 1000;
 	public static final int LOGIN_RESPONSE_OK = 2;
--- vscape Server/src/com/rs2/model/players/container/equipment/Equipment.java
+++ vscape Server/src/com/rs2/model/players/container/equipment/Equipment.java
@@ -51,7 +51,7 @@ public class Equipment {
 	private Player player;
 
 	private static String[] hideArms = {"robe", "top", "blouse", "shirt", "platebody", "brassard", "dragon chainbody", "rock-shell plate", "spined body", "zamorak d'hide", "guthix d'hide", "saradomin d'hide", "lunar torso", "snakeskin body", "torags body", "dharoks body", "guthans body", "initiate hauberk", "doctors' gown"};
-	private static String[] hideHairAndBeard = {"spiny helmet", "initiate sallet", "full helm", "saradomin full", "veracs helm", "guthans helm", "torags helm", "saradomin helm", "spined helm", "lunar helm", "h'ween mask", "gas mask"};
+	private static String[] hideHairAndBeard = {"dank mask", "spiny helmet", "initiate sallet", "full helm", "saradomin full", "veracs helm", "guthans helm", "torags helm", "saradomin helm", "spined helm", "lunar helm", "h'ween mask", "gas mask"};
 	private static String[] hideHair = {"cowl", "camel", "bandana", "decorative helm", "med helm", "coif", "hood", "bandanna", "berserker helm", "archer helm", "farseer helm", "warrior helm", "skeletal", "dharoks helm", "mask", "rock-shell helm", "void melee helm", "void mage helm", "void ranger helm", "3rd age mage hat"};
 	private static String[] hideBeard = {"facemask", "mime mask"};
 
